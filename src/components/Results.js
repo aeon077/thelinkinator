ab589c86ab27bba422eebbd6add666be6ec7892a
aeon077 <carolyn.schiltz@gmail.com>
2020-09-10 00:50:58 -0400

    edits to components, added table editor node module

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import Table from 'react-bootstrap/Table';
+import '../scss/custom.scss'
+
 // import BootstrapTable from 'react-bootstrap-table-next';
-import '../scss/custom.scss';
 // import axios from 'axios';
 
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as Searchbar } from './Searchbar';
 export { default as URLform } from './URLform';
 export { default as Results } from './Results';
+export { default as UrlCard } from './UrlCard';
